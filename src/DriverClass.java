0b617823c47ad764831fd4bb16d24d5ceb2b4dd7
Pooh <Pooh@DESKTOP-LHLD9RJ>
2023-05-30 16:53:43 +0530

    Static framework

---
+++ src/DriverClass.java
@@ -0,0 +1,14 @@
+
+
+import java.io.IOException;
+
+import testClassPackage.POST_TC_1;
+
+public class DriverClass {
+
+	public static void main(String[] args) throws IOException {
+		POST_TC_1.execute();
+
+	}
+
+}
